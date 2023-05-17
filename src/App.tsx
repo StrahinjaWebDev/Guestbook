3c9b3ef875d8bff4c39986170fa83eb57d8f6697
Strahinja Vujinovic <vujinovicstrahinja123@gmail.com>
2023-05-17 12:48:16 +0200

    setting up typescript, eslint, tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GuestBook from "./Guestbook";
+
+function App() {
+  return (
+    <>
+      <GuestBook />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Guestbook.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react";
+
+function GuestBook() {
+  return <div></div>;
+}
+
+export default GuestBook;
