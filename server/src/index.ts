94d02b98b364f1ef4354f0d982e8184b3a238f86
Strahinja Vujinovic <vujinovicstrahinja123@gmail.com>
2023-05-18 20:01:09 +0200

    setting up api for login

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,16 +3,39 @@ config();
 
 import express, { Request, Response } from "express";
 import { prisma } from "./utils/db";
+import cors from "cors";
 import { User } from "@prisma/client";
 
 const PORT = 5000;
 
 const app = express();
 
-app.get("/", async (req: Request, res: Response) => {
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+
+app.get("/users", async (req: Request, res: Response) => {
   const users = await prisma.user.findMany();
   res.json(users);
+  console.log(users);
 });
+
+app.post("/users/login"),
+  async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+    if (!user || user.password !== password) {
+      res.status(401).json({ error: "Invalid username or password" });
+    } else {
+      res.json({ message: "Success" });
+    }
+  };
 
 app.post("/", async (req: Request, res: Response) => {
   const newUser = await prisma.user.create({
@@ -24,4 +47,4 @@ app.post("/", async (req: Request, res: Response) => {
   });
 });
 
-app.listen(PORT, () => console.log("Miki"));
+app.listen(PORT, () => console.log("Log check"));
