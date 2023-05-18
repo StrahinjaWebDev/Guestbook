adf63c30eeb2fb1262ddf565b548698f97900f87
Strahinja Vujinovic <vujinovicstrahinja123@gmail.com>
2023-05-18 13:51:36 +0200

    adding prisma and setting up api for getting users

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,27 @@
+import { config } from "dotenv";
+config();
+
 import express, { Request, Response } from "express";
+import { prisma } from "./utils/db";
+import { User } from "@prisma/client";
+
+const PORT = 5000;
+
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Al");
+app.get("/", async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
 });
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Aa");
+app.post("/", async (req: Request, res: Response) => {
+  const newUser = await prisma.user.create({
+    data: {
+      id: req.body.id,
+      username: req.body.username,
+      password: req.body.password,
+    },
+  });
 });
 
-app.listen(5000);
+app.listen(PORT, () => console.log("Miki"));
