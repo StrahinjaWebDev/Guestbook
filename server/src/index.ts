0ec07939a559015e0d8a989ea15af3bd8104cc98
Strahinja Vujinovic <vujinovicstrahinja123@gmail.com>
2023-05-21 16:40:15 +0200

    refactor code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,11 @@
 import { config } from "dotenv";
 config();
 
-import express, { Request, Response } from "express";
+import express, { Request, Response, response } from "express";
 import { prisma } from "./utils/db";
 import jwt from "jsonwebtoken";
 import cors from "cors";
-import { v4 as uuidv4 } from "uuid";
-import { User } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import { Session } from "./model/Session";
 
 const PORT = 5000;
@@ -22,6 +21,8 @@ app.use(
 app.use(express.json());
 
 const sessions: Session = {};
+
+//* User
 
 app.get("/users", async (req: Request, res: Response) => {
   const users = await prisma.user.findMany();
@@ -52,15 +53,13 @@ app.post("/users/login", async (req: Request, res: Response) => {
   }
 });
 
-// console.log(process.env.TOKEN_SECRET);
-
-app.post("/users/logout", async (req: Request, res: Response) => {
-  const sessionId = req.headers.cookie?.split("=")[1];
-  if (sessionId) {
-    delete sessions[sessionId];
-  }
-  res.set("Set-Cookie", "session=; expires=Thu, 01 Jan 1970 00:00:00 GMT");
-});
+// app.post("/users/logout", async (req: Request, res: Response) => {
+//   const sessionId = req.headers.cookie?.split("=")[1];
+//   if (sessionId) {
+//     delete sessions[sessionId];
+//   }
+//   res.set("Set-Cookie", "session=; expires=Thu, 01 Jan 1970 00:00:00 GMT");
+// });
 
 app.post("/", async (req: Request, res: Response) => {
   const newUser = await prisma.user.create({
@@ -70,6 +69,50 @@ app.post("/", async (req: Request, res: Response) => {
       password: req.body.password,
     },
   });
+});
+
+//* Post
+
+app.get("/posts", async (req: Request, res: Response) => {
+  const posts = await prisma.post.findMany();
+  res.json(posts);
+  console.log(posts);
+});
+
+app.get("/posts/recent", async (req: Request, res: Response) => {
+  const posts = await prisma.post.findMany({
+    take: 10,
+    orderBy: { createdAt: "desc" } as Prisma.PostOrderByWithRelationInput,
+    include: {
+      author: true,
+    },
+  });
+
+  const postsWithAuthor = posts.map((post) => ({
+    ...post,
+    author: post.author.username,
+  }));
+
+  res.json(postsWithAuthor);
+});
+
+app.post("/posts", async (req: Request, res: Response) => {
+  const { content, email, authorId } = req.body;
+  try {
+    const newPost = await prisma.post.create({
+      data: {
+        content,
+        email,
+        authorId,
+      },
+    });
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the post" });
+  }
 });
 
 app.listen(PORT, () => console.log("Log check"));
