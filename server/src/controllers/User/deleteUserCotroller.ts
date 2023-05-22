043400caf8c778fa9f4dc16da656a6cca1ae5fa6
Strahinja Vujinovic <vujinovicstrahinja123@gmail.com>
2023-05-23 00:20:56 +0200

    fixing bug that user that posted can be deleted

--- server/src/controllers/User/deleteUserCotroller.ts
+++ server/src/controllers/User/deleteUserCotroller.ts
@@ -5,10 +5,19 @@ export async function deleteUserControler(req: Request, res: Response) {
   const { id } = req.params;
 
   try {
+    const userPosts = await prisma.post.findMany({
+      where: { authorId: id },
+    });
+
+    await prisma.post.deleteMany({
+      where: { authorId: id },
+    });
+
     const deletedUser = await prisma.user.delete({
       where: { id: id },
     });
-    res.status(200).json(deletedUser);
+
+    res.status(200).json({ deletedUser, deletedPosts: userPosts });
   } catch (error) {
     res
       .status(500)
